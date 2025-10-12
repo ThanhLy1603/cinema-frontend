1f20fddf0e2b966011d11ad9eba8d89086802828
ThanhLy1603 <Lynguyenthanh1603@gmail.com>
2025-10-12 23:35:19 +0700

    install bootstrap, router, axios.

---
+++ Frontend/src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+// Khai báo routes
+const routes = [
+
+];
+
+// Tạo router
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+
+export default router
