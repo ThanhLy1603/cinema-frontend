0fb27090902851aac821868356cb9ae380f0f987
ThanhLy1603 <Lynguyenthanh1603@gmail.com>
2025-10-12 23:58:08 +0700

    fix router

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,5 +3,5 @@ import './style.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
+import router from './router';
 createApp(App).use(router).mount('#app')
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -11,5 +11,4 @@ const router = createRouter({
   routes
 });
 
-
 export default router
