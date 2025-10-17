89ba41ab52b25627f7a1069ceb3d727443cdcf20
DESKTOP-OPGF56D\ASUS <truongthang2626@gmail.com>
2025-10-17 13:36:40 +0700

    theem the p

--- Frontend/src/components/HelloWorld.vue
+++ Frontend/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Hello World</h1>
-
+<p>lyx con chos</p>
   <img src="http://localhost:8080/uploads/images/Tenki_no_Ko_Poster.jpg"></img>
 
   <video src="http://localhost:8080/uploads/videos/tenkinoko.mp4" controls></video>
